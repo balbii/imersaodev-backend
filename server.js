80371a6d7971472116ecd2c91e40204c56abb612
balbii <renan.balbino56@gmail.com>
2024-11-22 20:42:40 -0300

    aula 2 - api e base de dados

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma linda paisagem",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Um delicioso prato",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Um animal fofo",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 4,
+        descricao: "Uma cidade vibrante",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 5,
+        descricao: "Um objeto de arte",
+        imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando");
 });
-// parametros (rota, (requisição, resposta))
-app.get("/api", (req, res) => {
-    res.status(200).send("Rota inicial.");
+// parametros do get(rota, (requisição, resposta))
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id)
+    res.status(200).json(posts[index]);
 });
