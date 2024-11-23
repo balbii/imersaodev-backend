ae0e80a7292f8e4cc33a8e59d5b32c799552f0f4
balbii <renan.balbino56@gmail.com>
2024-11-23 19:24:17 -0300

    aula 3 - refatorando código

--- server.js
+++ server.js
@@ -34,18 +34,3 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log("Servidor escutando");
 });
-// parametros do get(rota, (requisição, resposta))
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorId(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id)
-    res.status(200).json(posts[index]);
-});
